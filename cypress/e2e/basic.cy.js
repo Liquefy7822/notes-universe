c95f94a62d6784b0b5da04960caf38f0a57dd007
Emmanuel Yee <127311695+Liquefy7822@users.noreply.github.com>
2023-10-04 11:56:54 +0800

    jojo

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -1,17 +1,20 @@
 describe('sample test', () => {
   beforeEach(() => {
-    cy.visit('/')
-  })
+    // Visit the home page with the sidebar layout
+    cy.visit('/');
+  });
 
   it('displays the resources text', () => {
-    cy.get('h1')
-    .contains('Welcome to my app!');
-  })
+    // Assuming the main content area contains the title
+    cy.get('h1').contains('Home Page');
+  });
+
   it('renders the Netlify logo image', () => {
+    // Assuming the image is located within the main content area
     cy.get('img')
-    .should('be.visible')
-    .and(($img) => {
-      expect($img[0].naturalWidth).to.be.greaterThan(0);
-    })
-  })
-})
+      .should('be.visible')
+      .and(($img) => {
+        expect($img[0].naturalWidth).to.be.greaterThan(0);
+      });
+  });
+});
