eab2a5c2524f3a756a633c64836c0075cfb695e1
Emmanuel Yee <127311695+Liquefy7822@users.noreply.github.com>
2023-10-04 12:05:14 +0800

    jjgio

--- components/Layout.js
+++ components/Layout.js
@@ -1,32 +1,31 @@
 // components/Layout.js
 import React from 'react';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 import styles from './Layout.module.css'; // Import your CSS module
 
 const Layout = ({ children }) => {
+  const router = useRouter();
+
+  // Function to handle navigation
+  const navigateTo = (path) => {
+    router.push(path);
+  };
+
   return (
     <div className={styles.appContainer}>
       <nav className={styles.sideMenu}>
         <ul>
           <li>
-            <button>
-              <Link href="/">Home</Link>
-            </button>
+            <button onClick={() => navigateTo('/')}>Home</button>
           </li>
           <li>
-            <button>
-              <Link href="/login">Login</Link>
-            </button>
+            <button onClick={() => navigateTo('/login')}>Login</button>
           </li>
           <li>
-            <button>
-              <Link href="/signup">Signup</Link>
-            </button>
+            <button onClick={() => navigateTo('/signup')}>Signup</button>
           </li>
           <li>
-            <button>
-              <Link href="/profile">Profile</Link>
-            </button>
+            <button onClick={() => navigateTo('/profile')}>Profile</button>
           </li>
           {/* Add more menu items as buttons for other sections */}
         </ul>
