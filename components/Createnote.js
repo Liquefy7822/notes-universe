66569506811713a0c4fd4b8cc540a12ff08ed9fb
Emmanuel Yee <127311695+Liquefy7822@users.noreply.github.com>
2023-10-09 11:46:33 +0800

    nnk

---
+++ components/Createnote.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShareNotePage = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h1>Createnote</h1>
+    </div>
+  );
+};
+
+export default ShareNotePage;
